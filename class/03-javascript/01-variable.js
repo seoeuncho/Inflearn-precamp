d43d4bedc4407182d1bf4228882170daa80e2c2d
seoeuncho <westsilver24@naver.com>
2023-03-02 00:06:46 +0900

    변수와 상수

---
+++ class/03-javascript/01-variable.js
@@ -0,0 +1,12 @@
+// var 재선언, 재할당 가능
+
+// let 재선언 불가능, 재할당 가능
+let name = '조약돌'
+// let name = '홍길동' // 재선언 불가능, 재할당 가능
+console.log(name);
+
+// const 재선언, 재할당 불가능
+const gender = '홍길동';
+// const gender = '스티브'; // 재선언, 재할당 불가능
+// gender = '스티브'; // 재할당 불가능
+console.log(gender);
